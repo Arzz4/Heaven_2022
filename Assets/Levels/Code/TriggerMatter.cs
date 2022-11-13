68749575d925f44c72a08626ac9644f26bb120bf
Roxling <valdemar.roxling@gmail.com>
2022-11-13 12:56:39 +0100

    Matter player creates tiles on death

---
+++ Assets/Levels/Code/TriggerMatter.cs
@@ -0,0 +1,32 @@
+using AudioSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMatter : MonoBehaviour
+{
+    TileLogic tileLogic;
+    public float destroyAfterTime = 0.2f;
+    void Start()
+    {
+        tileLogic = GameObject.FindObjectOfType<TileLogic>();
+        if (tileLogic != null)
+        {
+            tileLogic.CreateTiles(transform.position);
+        }
+
+        AudioManager manager = AudioManager.Instance;
+        if (manager != null)
+        {
+            AudioDatabase db = manager.GetAudioDatabase();
+            manager.PlayOnShotAudioOnVFXAudioSource(db.GetWetSplatVFX(Random.Range(0, db.GetNumberOfWetSplatVFX())));
+        }
+
+        StartCoroutine(killMe());
+    }
+    private IEnumerator killMe()
+    {
+        yield return new WaitForSeconds(destroyAfterTime);
+        Destroy(gameObject);
+    }
+}
