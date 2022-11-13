e4079c5c70acd0561038c4665943e5517828fdc8
Ricardo David <deadtheory84@gmail.com>
2022-11-13 01:09:22 +0100

    player explosion audio hook

--- Assets/Audio/Code/AudioDatabase.cs
+++ Assets/Audio/Code/AudioDatabase.cs
@@ -5,6 +5,7 @@ namespace AudioSystems
 	[CreateAssetMenu(fileName = "Audio Clips Database", menuName = "Audio/New Audio Database")]
 	public class AudioDatabase : ScriptableObject
 	{
+		[Header("Levels")]
 		[SerializeField]
 		private AudioClip m_DefaultMainMenuAudio = default;
 
@@ -14,13 +15,15 @@ namespace AudioSystems
 		[SerializeField]
 		private AudioClip[] m_ScenesAudioClips = default;
 
+		[Header("Player VFX")]
 		[SerializeField]
-		private AudioClip m_OnTileClicked = default;
-		public AudioClip OnTileClicked { get { return m_OnTileClicked; } }
+		private AudioClip m_PlayerExplodeVFX = default;
+		public AudioClip PlayerExplodeVFX { get { return m_PlayerExplodeVFX; } }
 
 		[SerializeField]
-		private AudioClip m_OnLevelCompleted = default;
-		public AudioClip OnLevelCompleted { get { return m_OnLevelCompleted; } }
+		private AudioClip[] m_WetSplatVFX = default;
+		public int GetNumberOfWetSplatVFX() { return m_WetSplatVFX.Length; }
+		public AudioClip GetWetSplatVFX(int index) { return m_WetSplatVFX[index];}
 
 		public AudioClip GetBackgroundAudioForScene(int aSceneBuildIndex)
 		{
