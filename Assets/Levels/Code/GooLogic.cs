3851d36b3f2207aabc9036a349407dac68612d91
Roxling <valdemar.roxling@gmail.com>
2022-11-13 13:44:56 +0100

    Speedy logic

--- Assets/Levels/Code/GooLogic.cs
+++ Assets/Levels/Code/GooLogic.cs
@@ -1,34 +1,3 @@
-using UnityEngine;
-using UnityEngine.Tilemaps;
-using Tile = UnityEngine.Tilemaps.Tile;
-
-public class GooLogic : MonoBehaviour
+public class GooLogic : SprayLogic
 {
-
-    private Tilemap tiles;
-    public Sprite sprite;
-    // Start is called before the first frame update
-    void Start()
-    {
-        tiles = GetComponent<Tilemap>();
-    }
-
-    public void createGoo(Vector3 worldPosition, Vector3 vector3)
-    {
-        Vector3Int local = tiles.WorldToCell(worldPosition);
-        Tile tile = ScriptableObject.CreateInstance<Tile>();
-        tile.sprite = sprite;
-        float rot = 0;
-        if (Mathf.Abs(vector3.x) > Mathf.Abs(vector3.y))
-        {
-            rot = vector3.x > 0 ? -90 : 90;
-        }
-        else
-        {
-            rot = vector3.y > 0 ? 0 : 180;
-        }
-        tile.transform = Matrix4x4.Rotate(Quaternion.Euler(new Vector3(0,0,rot)));
-        tiles.SetTile(local, tile);
-        tiles.RefreshTile(local);
-    }
 }
