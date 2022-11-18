f204a49da1ce46847b42418aa97b670fa46a0c0f
Ricardo David <deadtheory84@gmail.com>
2022-11-18 23:26:06 +0100

    deleted unused code

--- Assets/Levels/Code/Level.cs
+++ Assets/Levels/Code/Level.cs
@@ -1,8 +1,5 @@
 using PlayerPlatformer2D;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class Level : MonoBehaviour
 {
@@ -19,20 +16,5 @@ public class Level : MonoBehaviour
         psxTiles = GameObject.FindObjectOfType<TileLogic>();
         player = GameObject.FindObjectOfType<PlayerCharacter>();
         end = GameObject.FindObjectOfType<EndTrigger>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //    psxTiles.RemoveTiles(pos);
-        //}
-        //if (Input.GetMouseButtonDown(1))
-        //{
-        //    Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //    psxTiles.TintTiles(pos);
-        //}
     }
 }
